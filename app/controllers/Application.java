5360c5467f82f317b334ead009ecb608aa4412e2
Marc <marc.maury@gmail.com>
2012-09-20 11:49:37 +0200

    test+dependences

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,27 @@
 package controllers;
 
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
+import com.hp.hpl.jena.rdf.model.*;
+
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+
+	public static Result index() {
+		Model m = ModelFactory.createDefaultModel();
+		String NS = "http://exemple.com/";
+		
+		Resource r = m.createResource(NS + "r");
+		Property p = m.createProperty(NS + "p");
+		
+		r.addProperty(p, "hello world", XSDDatatype.XSDstring);
+		
+		m.write(System.out, "Turtle");
+		
+		return ok(index.render("Your new application is ready."));
+	}
+
 }
--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,18 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "Geonigme"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName = "Geonigme"
+  val appVersion = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+    "org.apache.jena" % "jena-core" % "2.7.3",
+    "org.apache.jena" % "jena-tdb" % "0.9.3",
+    "org.apache.jena" % "jena-arq" % "2.9.3" 
+    // Add your project dependencies here
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings( 
+  	// Add your own project settings here      
+  )
 
 }
