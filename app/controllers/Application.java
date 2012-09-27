ff2bb35aeff461d127e5aae4a87410f978160658
mmaury <mmaury@t15.etu.info-ufr.univ-montp2.fr>
2012-09-27 16:45:30 +0200

    formulaires login/inscription

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,46 +6,58 @@ import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.object.ObjectConnection;
 
+import forms.*;
 import global.Sesame;
 import models.Hunt;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-	return ok(index.render());
-    }
-    
-    public static Result submitContactForm() {
-	return ok();
-    }
-    
-    public static Result test1() throws RepositoryException {
-	Hunt h = new Hunt();
-	h.setLevel(3);
-	h.setPublished(false);
-
-	ObjectConnection oc = Sesame.getObjectConnection();
-	oc.clear();
-	oc.addObject(h);
-
-	return ok();
-    }
-
-    public static Result test2() throws RepositoryException,
-	    QueryEvaluationException {
-	ObjectConnection oc = Sesame.getObjectConnection();
-
-	Set<gngm.Hunt> result = oc.getObjects(gngm.Hunt.class).asSet();
-
-	for (gngm.Hunt h : result) {
-	    System.out.println(h.getLevel());
-	    System.out.println(h.getPublished());
+	public static Result index() {
+		return ok(index.render());
 	}
 
-	return ok();
-    }
+	public static Result submitContactForm() {
+		// TODO
+		return ok();
+	}
+	
+	public static Result login() {
+		Form<Login> formLogin = form(Login.class);
+		Form<Register> formRegister = form(Register.class);
+		
+		formRegister.bindFromRequest().ge
+		
+		return ok(login.render(formLogin, formRegister));
+	}
+
+	public static Result test1() throws RepositoryException {
+		Hunt h = new Hunt();
+		h.setLevel(3);
+		h.setPublished(false);
+
+		ObjectConnection oc = Sesame.getObjectConnection();
+		oc.clear();
+		oc.addObject(h);
+
+		return ok();
+	}
+
+	public static Result test2() throws RepositoryException,
+			QueryEvaluationException {
+		ObjectConnection oc = Sesame.getObjectConnection();
+
+		Set<gngm.Hunt> result = oc.getObjects(gngm.Hunt.class).asSet();
+
+		for (gngm.Hunt h : result) {
+			System.out.println(h.getLevel());
+			System.out.println(h.getPublished());
+		}
+
+		return ok();
+	}
 
 }
