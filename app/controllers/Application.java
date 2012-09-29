46c9a22754a7fa5b1c3e1f8aaab1503ea2005394
Gaétan Lamoureux <lam.gaetan@gmail.com>
2012-09-29 10:08:45 +0200

    controller manager

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -15,7 +15,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-	return ok(index.render());
+    	return ok(index.render(null));
     }
     
     public static Result submitContactForm() {
